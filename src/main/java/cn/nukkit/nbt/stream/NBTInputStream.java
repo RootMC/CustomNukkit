692bb6a72ce348f3dc1c0489454c5a3fe4099b32
Khanh Le <bacdau852@gmail.com>
2020-05-01 00:57:10 +0700

    Fix NBT loading errors

--- src/main/java/cn/nukkit/nbt/stream/NBTInputStream.java
+++ src/main/java/cn/nukkit/nbt/stream/NBTInputStream.java
@@ -82,11 +82,7 @@ public class NBTInputStream implements DataInput, AutoCloseable {
 
     @Override
     public int readUnsignedShort() throws IOException {
-        int s = this.stream.readUnsignedShort();
-        if (endianness == ByteOrder.LITTLE_ENDIAN) {
-            s = Integer.reverseBytes(s) >> 16;
-        }
-        return s;
+        return this.readShort() & 0xFFFF;
     }
 
     @Override
@@ -148,7 +144,7 @@ public class NBTInputStream implements DataInput, AutoCloseable {
 
     @Override
     public String readUTF() throws IOException {
-        int length = (int) (network ? VarInt.readUnsignedVarInt(stream) : this.readUnsignedShort());
+        int length = network ? (int) VarInt.readUnsignedVarInt(stream) : this.readUnsignedShort();
         byte[] bytes = new byte[length];
         this.stream.read(bytes);
         return new String(bytes, StandardCharsets.UTF_8);
