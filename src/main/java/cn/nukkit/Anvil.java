4f64b8bdff6ee0bfce7fa3a793db5c2c76120182
Khanh Le <bacdau852@gmail.com>
2020-05-08 06:54:25 +0700

    Anvil Test

---
+++ src/main/java/cn/nukkit/Anvil.java
@@ -0,0 +1,68 @@
+package cn.nukkit;
+
+import cn.nukkit.block.Block;
+import cn.nukkit.event.EventHandler;
+import cn.nukkit.event.inventory.InventoryCloseEvent;
+import cn.nukkit.event.player.PlayerInteractEvent;
+import cn.nukkit.event.server.DataPacketReceiveEvent;
+import cn.nukkit.item.Item;
+import cn.nukkit.network.protocol.InventoryTransactionPacket;
+import cn.nukkit.network.protocol.types.NetworkInventoryAction;
+import cn.nukkit.inventory.AnvilInventory;
+
+import java.util.HashMap;
+
+public class Anvil {
+
+    //RootNetwork Anvil
+
+    public HashMap<String, AnvilInventory> anvils = new HashMap<>();
+
+    @EventHandler
+    public void onInteract(PlayerInteractEvent event) {
+        if(event.getBlock().getId() == Block.ANVIL)
+            anvils.put(event.getPlayer().getName(), new AnvilInventory(event.getPlayer().getUIInventory(), event.getBlock()));
+    }
+
+    @EventHandler
+    public void onClose(InventoryCloseEvent event) {
+        anvils.remove(event.getPlayer().getName());
+    }
+
+    @EventHandler
+    public void onReceive(DataPacketReceiveEvent event) {
+        if(!(event.getPacket() instanceof InventoryTransactionPacket)) {
+            return;
+        }
+
+        Player player = event.getPlayer();
+
+        if(!anvils.containsKey(player.getName())) {
+            return;
+        }
+
+        InventoryTransactionPacket packet = (InventoryTransactionPacket) event.getPacket();
+        AnvilInventory inventory = anvils.get(event.getPlayer().getName());
+
+        boolean isFromInv = false;
+
+        for(NetworkInventoryAction action : packet.actions) {
+            if(action.windowId == 0 && !isFromInv) {
+                isFromInv = true;
+            }
+
+            if(isFromInv && action.windowId == 124 && action.inventorySlot > 0 && action.inventorySlot < 3) {
+                inventory.setItem(action.inventorySlot - 1, action.newItem);
+                player.getInventory().removeItem(action.newItem);
+            }
+
+            if(action.windowId == -12 && action.inventorySlot == 2 && action.newItem.getId() == 0) {
+                if(inventory.onRename(event.getPlayer(), action.oldItem, false)) {
+                    for(Item item : inventory.getContents().values()) {
+                        player.getUIInventory().removeItem(item);
+                    }
+                }
+            }
+        }
+    }
+}
--- src/main/java/cn/nukkit/inventory/AnvilInventory.java
+++ src/main/java/cn/nukkit/inventory/AnvilInventory.java
@@ -8,9 +8,11 @@ import cn.nukkit.level.Position;
 import cn.nukkit.nbt.tag.CompoundTag;
 import cn.nukkit.network.protocol.LevelSoundEventPacket;
 import io.netty.util.internal.StringUtil;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Iterator;
 import java.util.Map;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.stream.Stream;
@@ -382,5 +384,105 @@ public class AnvilInventory extends FakeBlockUIComponent {
             default:
                 return 0;
         }
+    }
+    //RootNetwork Anivil
+    public boolean onRename(Player player, @NotNull Item resultItem, boolean sendToUI) {
+        Item local = getItem(TARGET);
+        Item second = getItem(SACRIFICE);
+
+        if (!resultItem.equals(local, true, false) || resultItem.getCount() != local.getCount()) {
+            //Item does not match target item. Everything must match except the tags.
+            return false;
+        }
+
+        if (local.equals(resultItem)) {
+            //just item transaction
+            return true;
+        }
+
+        if (local.getId() != 0 && second.getId() == 0) { //only rename
+            local.setCustomName(resultItem.getCustomName());
+            setItem(RESULT, local);
+            player.getInventory().addItem(local);
+            clearAll();
+            player.getInventory().sendContents(player);
+            sendContents(player);
+
+            player.getLevel().addLevelSoundEvent(player, LevelSoundEventPacket.SOUND_RANDOM_ANVIL_USE);
+            return true;
+        } else if (local.getId() != 0 && second.getId() != 0) { //enchants combining
+            if (!local.equals(second, true, false)) {
+                return false;
+            }
+
+            if (local.getId() != 0 && second.getId() != 0) {
+                Item result = local.clone();
+                int enchants = 0;
+
+                ArrayList<Enchantment> enchantments = new ArrayList<>(Arrays.asList(second.getEnchantments()));
+
+                ArrayList<Enchantment> baseEnchants = new ArrayList<>();
+
+                for (Enchantment ench : local.getEnchantments()) {
+                    if (ench.isMajor()) {
+                        baseEnchants.add(ench);
+                    }
+                }
+
+                for (Enchantment enchantment : enchantments) {
+                    if (enchantment.getLevel() < 0 || enchantment.getId() < 0) {
+                        continue;
+                    }
+
+                    if (enchantment.isMajor()) {
+                        boolean same = false;
+                        boolean another = false;
+
+                        for (Enchantment baseEnchant : baseEnchants) {
+                            if (baseEnchant.getId() == enchantment.getId())
+                                same = true;
+                            else {
+                                another = true;
+                            }
+                        }
+
+                        if (!same && another) {
+                            continue;
+                        }
+                    }
+
+                    Enchantment localEnchantment = local.getEnchantment(enchantment.getId());
+
+                    if (localEnchantment != null) {
+                        int level = Math.max(localEnchantment.getLevel(), enchantment.getLevel());
+
+                        if (localEnchantment.getLevel() == enchantment.getLevel())
+                            level++;
+
+                        enchantment.setLevel(level);
+                        result.addEnchantment(enchantment);
+                        continue;
+                    }
+
+                    result.addEnchantment(enchantment);
+                    enchants++;
+                }
+
+                if(!resultItem.getCustomName().equals("")) {
+                    result.setCustomName(resultItem.getCustomName());
+                }
+
+                player.getInventory().addItem(result);
+                player.getInventory().sendContents(player);
+
+                clearAll();
+                sendContents(player);
+
+                player.getLevel().addLevelSoundEvent(player, LevelSoundEventPacket.SOUND_RANDOM_ANVIL_USE);
+                return true;
+            }
+        }
+
+        return false;
     }
 }
